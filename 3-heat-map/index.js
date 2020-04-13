26f9268354b4ecd5761ba65a3d1f16e74d7e05e7
liamst19 <liamtstko20@gmail.com>
2020-04-13 20:15:59 +0900

    Project 3: Heat Map - colors seem to be working

--- 3-heat-map/index.js
+++ 3-heat-map/index.js
@@ -11,31 +11,6 @@ const svgDimensions = {
   }
 }
 
-var colorbrewer = {
-  RdYlBu: {
-    3: ["#fc8d59","#ffffbf","#91bfdb"],
-    4: ["#d7191c","#fdae61","#abd9e9","#2c7bb6"],
-    5: ["#d7191c","#fdae61","#ffffbf","#abd9e9","#2c7bb6"],
-    6: ["#d73027","#fc8d59","#fee090","#e0f3f8","#91bfdb","#4575b4"],
-    7: ["#d73027","#fc8d59","#fee090","#ffffbf","#e0f3f8","#91bfdb","#4575b4"],
-    8: ["#d73027","#f46d43","#fdae61","#fee090","#e0f3f8","#abd9e9","#74add1","#4575b4"],
-    9: ["#d73027","#f46d43","#fdae61","#fee090","#ffffbf","#e0f3f8","#abd9e9","#74add1","#4575b4"],
-    10: ["#a50026","#d73027","#f46d43","#fdae61","#fee090","#e0f3f8","#abd9e9","#74add1","#4575b4","#313695"],
-    11: ["#a50026","#d73027","#f46d43","#fdae61","#fee090","#ffffbf","#e0f3f8","#abd9e9","#74add1","#4575b4","#313695"]
-  },
-  RdBu: {
-    3: ["#ef8a62","#f7f7f7","#67a9cf"],
-    4: ["#ca0020","#f4a582","#92c5de","#0571b0"],
-    5: ["#ca0020","#f4a582","#f7f7f7","#92c5de","#0571b0"],
-    6: ["#b2182b","#ef8a62","#fddbc7","#d1e5f0","#67a9cf","#2166ac"],
-    7: ["#b2182b","#ef8a62","#fddbc7","#f7f7f7","#d1e5f0","#67a9cf","#2166ac"],
-    8: ["#b2182b","#d6604d","#f4a582","#fddbc7","#d1e5f0","#92c5de","#4393c3","#2166ac"],
-    9: ["#b2182b","#d6604d","#f4a582","#fddbc7","#f7f7f7","#d1e5f0","#92c5de","#4393c3","#2166ac"],
-    10: ["#67001f","#b2182b","#d6604d","#f4a582","#fddbc7","#d1e5f0","#92c5de","#4393c3","#2166ac","#053061"],
-    11: ["#67001f","#b2182b","#d6604d","#f4a582","#fddbc7","#f7f7f7","#d1e5f0","#92c5de","#4393c3","#2166ac","#053061"]
-  }
-};
-
 // Run when the whole page DOM has been loaded from the html file.
 document.addEventListener('DOMContentLoaded', ()=> {
   d3.json(DATA_URL).then(json => {
@@ -46,8 +21,8 @@ document.addEventListener('DOMContentLoaded', ()=> {
     year: 1753
   }
 */
-    const data = json.monthlyVariance;
-    const baseTemperature = json.baseTemperature;
+    const baseTemp = json.baseTemperature;
+    const data = json.monthlyVariance.map(d => ({...d, temperature: parseFloat((d.variance + baseTemp).toPrecision(5))}));
 
     const minYear = new Date(d3.min(data, d => d.year), 0, 1);
     const maxYear = new Date(d3.max(data, d => d.year), 0, 1);
@@ -56,12 +31,12 @@ document.addEventListener('DOMContentLoaded', ()=> {
     const years = data.reduce((domain, d) => domain.some(m => m === d.year)
                               ? domain
                               : [...domain, d.year] ,[]);
-    const variances = data.map(d => d.variance)
+    const temperatures = data.map(d => d.temperature);
     const legendCellSize = 30;
     const cellCount = 11;
 
-    const minTemp = d3.min(variances, v => v + baseTemperature);
-    const maxTemp = d3.max(variances, v => v + baseTemperature);
+    const minTemp = d3.min(temperatures);
+    const maxTemp = d3.max(temperatures);
 
     const bandWidthY = svgDimensions.height / months.length;
     const bandWidthX = 5;
@@ -100,9 +75,12 @@ document.addEventListener('DOMContentLoaded', ()=> {
     const yScale = d3.scaleBand()
           .domain(months)
           .range([0, svgDimensions.height]);
-    const varianceScale = d3.scaleThreshold()
-          .domain(variances)
-          .range(colorbrewer.RdYlBu[cellCount].reverse())
+
+    const tempDomain = [...Array(cellCount).keys()].map(i => (i + minTemp) * ((maxTemp - minTemp) / cellCount));
+    const colors = d3.schemeRdYlBu[cellCount].reverse();
+    const tempColor = d3.scaleThreshold()
+          .domain(tempDomain)
+          .range(colors)
 
     // Axes
     const xAxis = d3.axisBottom(xScale)
@@ -126,27 +104,30 @@ document.addEventListener('DOMContentLoaded', ()=> {
     const legendY = offsetY + svgDimensions.margin.top + svgDimensions.margin.bottom;
     const legendX = svgDimensions.margin.left;
 
-    const legend = svg.append('g')
-          .attr('id', 'legend')
-          .attr('transform', `translate(${legendX}, ${legendY})`)
-          .selectAll('rect')
-          .data(varianceScale.range())
-          .enter()
-          .append('rect')
-          .attr('fill', d => d)
-          .attr('width', legendCellSize)
-          .attr('height', legendCellSize)
-          .attr('x', (d, i) => i * legendCellSize)
-
     const legendScale = d3.scaleLinear()
-          .domain(d3.extent([minTemp, maxTemp]))
+          .domain([minTemp, maxTemp])
           .range([0, legendCellSize * cellCount]);
+
     const legendAxis = d3.axisBottom(legendScale)
+          .tickValues(tempDomain)
+          .tickFormat(d3.format(".1f"))
 
     svg.append('g')
       .attr('id', 'legend-axis')
       .attr('transform', `translate(${legendX}, ${legendY + legendCellSize})`)
       .call(legendAxis)
+
+    svg.append('g')
+      .attr('id', 'legend')
+      .attr('transform', `translate(${legendX}, ${legendY})`)
+      .selectAll('rect')
+      .data(tempColor.range())
+      .enter()
+      .append('rect')
+      .attr('fill', d => d)
+      .attr('width', legendCellSize)
+      .attr('height', legendCellSize)
+      .attr('x', (d, i) => i * legendCellSize)
 
     // Chart
     const chart = svg.append('g')
@@ -158,13 +139,13 @@ document.addEventListener('DOMContentLoaded', ()=> {
           .append('rect')
           .attr('data-year', d => d.year)
           .attr('data-month', d => d.month - 1)
-          .attr('data-temp', d => baseTemperature + d.variance)
+          .attr('data-temp', d => d.temperature)
           .attr('class', 'cell')
           .attr('x', d => xScale(d.year))
           .attr('y', d => yScale(new Date(1600, d.month, 0)))
           .attr('width', bandWidthX)
           .attr('height', bandWidthY)
-          .attr('fill', d => varianceScale(d.variance))
+          .attr('fill', d => tempColor(d.temperature))
           .on('mouseover', (d, i) => {
             const pos = getTooltipPos(d3.event.pageX, d3.event.pageY);
             tooltip
@@ -173,7 +154,7 @@ document.addEventListener('DOMContentLoaded', ()=> {
               .style('visibility', 'visible')
               .style('left', `${pos.x}px`)
               .style('top', `${pos.y}px`)
-              .text(`${d.month}/${d.year} ${baseTemperature + d.variance}`)
+              .text(`${d.month}/${d.year} ${d.temperature}`)
           })
           .on('mouseout', () => {
             tooltip.style('visibility', 'hidden')
